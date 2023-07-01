007d5bde91bf18efdc73eac26ef971b5cfbfc2b9
Luis Henrique Jacinto <luis.silva@datamex.com.br>
2023-07-01 02:04:17 -0300

    feat: validação do body agora é um middleware

---
+++ src/utilities/make-request-body-validation-middleware-and-handler.ts
@@ -0,0 +1,34 @@
+import { IRouter, NextFunction, Request, RequestHandler, Response } from 'express'
+
+import { handleResponseError } from '.'
+import { JSONSchemaType } from '../types/json-types'
+import { validateJson } from './validate-json'
+import { InvalidRequestBodyError } from './errors'
+
+export function makeRequestBodyValidationMiddlewareAndHandler<T, K extends Request<any, any, T>, J extends Response>(
+  schema: JSONSchemaType<T>,
+  nextHandler: (
+    req: K,
+    res: J
+  ) => Promise<void>
+): [middleware: RequestHandler, handler: RequestHandler] {
+
+  const validateRequestBodyMiddleware = async (
+    req: K,
+    res: J,
+    next: NextFunction
+  ) => {
+    try {
+      validateJson<T>(schema, (req.body as Object))
+
+      return next()
+    } catch (error) {
+      const messageReplacingInstanceWithBody = (error as Error).message.split('instance').join('body')
+      handleResponseError(res, 401, new InvalidRequestBodyError(messageReplacingInstanceWithBody))
+    }
+  }
+
+  return [validateRequestBodyMiddleware as unknown as RequestHandler, nextHandler as unknown as RequestHandler]
+}
+
+
