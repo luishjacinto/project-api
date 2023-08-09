8e7175ec65bb5b0632084ebe9b83aa6ea1aaf287
Luis Henrique Jacinto <luis.silva@datamex.com.br>
2023-08-09 00:13:48 -0300

    feat: método para encerrar conexão com o banco

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,13 +1,17 @@
 import * as Mongoose from "mongoose"
 
-export const connect = () => {
+export const connect = async () => {
   const MONGO_URI: string = process.env.MONGO_URI || ''
 
   if (!MONGO_URI) {
     throw new Error("No MONGO_URI on environment variables")
   }
 
-  Mongoose.connect(MONGO_URI)
+  await Mongoose.connect(MONGO_URI)
     .then(() => console.log(`Connected to database ${MONGO_URI}`))
     .catch((error) => console.log(error))
+}
+
+export const close = async () => {
+  await Mongoose.connection?.close()
 }
